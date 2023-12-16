e2c07bff39e24dd48ff1dd3932c6411084ea75e3
Sashaaapp <sashaaapp1980@gmail.com>
2023-12-16 23:49:58 +0200

    js-more_upd

--- js/task-04.js
+++ js/task-04.js
@@ -1,1 +1,24 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var loginForm = document.querySelector('.login-form');
 
+    loginForm.addEventListener('submit', function (event) {
+      event.preventDefault();
+
+      var emailInput = loginForm.elements['email'];
+      var passwordInput = loginForm.elements['password'];
+
+      if (!emailInput.value.trim() || !passwordInput.value.trim()) {
+        alert('All form fields must be filled in');
+        return;
+      }
+
+      var formData = {
+        email: emailInput.value.trim(),
+        password: passwordInput.value.trim()
+      };
+
+      console.log(formData);
+
+      loginForm.reset();
+    });
+  });
